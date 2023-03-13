90994a76c82b779e6405788a103dc47f589b6189
ben27bryant <127363256+ben27bryant@users.noreply.github.com>
2023-03-13 12:56:17 -0300

    Added in LavaBall (Beta) and backround tiles
    Added backround sprites and startbutton screen at start.

---
+++ Dino Devastation/Assets/Scripts/LavaBallScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaBallScript : MonoBehaviour
+{
+    public float ballSpeed = 2f;
+
+    //void goUp() {
+        //if (this.Transform.position.y < 3f) {
+            //Transform.position = new Vector3(0.2f, Transform.position.y + ballSpeed + Time.deltaTime,0f);
+        //}
+    //}
+    
+    void Update()
+    {
+        //goUp();
+    }
+}
