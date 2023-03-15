4c15e0c9ab18c0218f11ed01679dcb4a7085fc91
jfougy <fjf142424@gmail.com>
2023-03-15 16:47:02 -0300

    Initial scene
    Initial scene menu and updates to button manager

--- Dino Devastation/Assets/Scripts/ButtonManager.cs
+++ Dino Devastation/Assets/Scripts/ButtonManager.cs
@@ -6,7 +6,17 @@ using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour
 {
     public void changeScene() {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("Initial Scene");
     }
+    public void GM1() {
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+    }
+    public void GM2() {
+        SceneManager.LoadScene("gm2", LoadSceneMode.Single);
+    }
+    public void Exit() {
+        Application.Quit();
+    }
+
 
 }
