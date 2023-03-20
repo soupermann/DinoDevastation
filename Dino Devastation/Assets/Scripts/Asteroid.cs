51ea1b72e49057ea3dff7b5ec9d2eb9311706bb0
jfougy <fjf142424@gmail.com>
2023-03-20 14:08:10 -0300

    Respawn and throw cam
    Testing only on 1v1 game mode atm

--- Dino Devastation/Assets/Scripts/Asteroid.cs
+++ Dino Devastation/Assets/Scripts/Asteroid.cs
@@ -7,20 +7,20 @@ public class Asteroid : MonoBehaviour
 {
 	[HideInInspector] public Rigidbody2D rb;
 	[HideInInspector] public CircleCollider2D col;
+	
 
 	[HideInInspector] public Vector3 pos { get { return transform.position; } }
-
+    public Camera mainCamera;
+	public Camera secondCamera;
 	public float damage = 10f;
 	public static float counter;
 	public static float counter2;
 	public static string instance;
-
+    public bool respawnOnlyWhenThrown = true;
 	public HealthManager healthManager;
 	public HealthManager healthManager0;
 	public HealthManager healthManager1;
 	public HealthManager healthManager2;
-
-
 	
 
 	void Awake()
@@ -45,6 +45,101 @@ public class Asteroid : MonoBehaviour
 		rb.angularVelocity = 0f;
 		rb.isKinematic = true;
 	}
+	public float respawnTime = 2f;
+    public float respawnOffset = 0.5f;
+    public float respawnThreshold = 0.01f;
+    private bool thrown = false;
+
+    
+    private Vector3 startPos;
+    private Quaternion startRot;
+    private float timeSinceStop;
+    private bool isMoving;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        startPos = transform.position;
+        startRot = transform.rotation;
+        timeSinceStop = 0f;
+        isMoving = false;
+    }
+	public void ThrowObject(Vector3 force)
+    {
+        rb.AddForce(force, ForceMode2D.Impulse);
+        thrown = true; // Mark the object as thrown
+    }
+
+    void Update()
+    {
+        // Check if the object is moving
+        if (rb.velocity.magnitude > respawnThreshold)
+        {
+            timeSinceStop = 0f;
+            isMoving = true;
+			
+        }
+        else
+        {
+            timeSinceStop += Time.deltaTime;
+            isMoving = false;
+        }
+
+        // Check if the object is offscreen
+        if (mainCamera != null)
+        {
+            Vector3 screenPos = mainCamera.WorldToViewportPoint(transform.position);
+            if (screenPos.x < 0f || screenPos.x > 1f || screenPos.y < 0f || screenPos.y > 1f)
+            {
+				thrown=true;
+				gameObject.SetActive(false);
+                Respawn();
+            }
+        }
+
+        // Respawn the object if it has stopped moving for the specified time
+        if (!isMoving &&  timeSinceStop  >= respawnTime && thrown == true)
+        {
+			thrown=true;
+			gameObject.SetActive(false);
+            Respawn();
+			
+        }
+    }
+	IEnumerator RespawnCoroutine(float timeToRespawn)
+    {
+        
+         if (thrown){ 
+		
+		
+		yield return new WaitForSeconds(timeToRespawn);
+		gameObject.SetActive(true);
+		transform.position = startPos;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = 0f;
+		thrown = false;
+		secondCamera.enabled = true;
+		mainCamera.enabled = false;
+	
+		
+		 }
+    }
+
+    public void Respawn()
+    {
+		//Destroy(GameObject.FindWithTag("Asteroid"));
+		gameObject.SetActive(false);
+		mainCamera.enabled = true;
+		MonoBehaviour camMono = Camera.main.GetComponent<MonoBehaviour>();
+//Use it to start your coroutine function
+        camMono.StartCoroutine(RespawnCoroutine(2f));
+		
+        
+        
+		
+        
+		
+    }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
---
+++ Dino Devastation/Assets/Scripts/LastThrow.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LastThrow : MonoBehaviour
+{
+    public Transform throwableObject;
+    public Camera mainCamera;
+    public Camera zoomCamera;
+    public float delay = 2f;
+
+    private bool switched = false;
+    private float timeSinceSpawned = 0f;
+    private bool waitingToSwitchBack = false;
+    private float timeSinceDestroyed = 0f;
+
+    public float zoomSpeed = 2f;
+    public float maxZoom = 5f;
+    public float minZoom = 1f;
+    public float zoomAmount = 2.0f;
+    public float zoomTime = 2.0f;
+
+    private float currentZoom = 1f;
+    private bool isZooming = false;
+    private float zoomStartTime;
+    private Vector3 originalPosition;
+
+    void Update()
+    {
+        if (throwableObject == null)
+        {
+            timeSinceDestroyed += Time.deltaTime;
+
+            if (!waitingToSwitchBack && timeSinceDestroyed >= delay)
+            {
+                mainCamera.enabled = true;
+                zoomCamera.enabled = false;
+                waitingToSwitchBack = true;
+                switched = false;
+            }
+        }
+        else
+        {
+            timeSinceSpawned += Time.deltaTime;
+
+            if (!switched && timeSinceSpawned >= delay)
+            {
+                mainCamera.enabled = false;
+                zoomCamera.enabled = true;
+                switched = true;
+                waitingToSwitchBack = false;
+                timeSinceDestroyed = 0f;
+            }
+        }// Check if the throwable object exists
+        if (throwableObject == null){
+            mainCamera.enabled = true;
+            zoomCamera.enabled = false;
+            return;
+        }
+        
+            
+
+        // Calculate the distance between the second camera and the throwable object
+        float distance = Vector3.Distance(zoomCamera.transform.position, throwableObject.position);
+
+        // Zoom in or out based on the distance
+        if (distance > maxZoom)
+        {
+            currentZoom -= zoomSpeed * Time.deltaTime;
+        }
+        else if (distance < minZoom)
+        {
+            currentZoom += zoomSpeed * Time.deltaTime;
+        }
+
+        // Clamp the zoom to the min and max values
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+        // Set the second camera's position and field of view based on the zoom
+        zoomCamera.transform.position = throwableObject.position - zoomCamera.transform.forward * currentZoom;
+        zoomCamera.fieldOfView = currentZoom * 10f;
+        if (isZooming)
+        {
+            float zoomProgress = (Time.time - zoomStartTime) / zoomTime;
+            if (zoomProgress >= 1.0f)
+            {
+                zoomCamera.transform.localPosition = originalPosition;
+                isZooming = false;
+            }
+            else
+            {
+                float currentZoom = Mathf.Lerp(0.0f, zoomAmount, zoomProgress);
+                zoomCamera.transform.localPosition = originalPosition + Vector3.back * currentZoom;
+            }
+        }
+    }
+
+    
+        public void StartZoom()
+    {
+        isZooming = true;
+        zoomStartTime = Time.time + 2.0f;
+        originalPosition = zoomCamera.transform.localPosition;
+    }
+}
